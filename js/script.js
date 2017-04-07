3c972961fc31e1c758c25b2766da456d41d387d0
sanjeev rao <sanjeev rao>
2017-04-07 19:45:46 +0530

    week5_commit4

--- js/script.js
+++ js/script.js
@@ -82,11 +82,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // On first load, show home view
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
-  allCategoriesUrl,
-  function (buildAndShowHomeHTML) {
-    document.querySelector("#main-content")
-      .innerHTML = buildAndShowHomeHTML;
-      }, // ***** <---- TODO: STEP 1: Substitute [...] ******
+  allCategoriesUrl,buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
@@ -118,7 +114,9 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-       var homeHtmlToInsertIntoMainPage = insertProperty ("homeHtml", "{{randomCategoryShortName}}", "chosenCategoryShortName");
+
+       var x = "'" + chosenCategoryShortName + "'";
+       var homeHtmlToInsertIntoMainPage = insertProperty (homeHtml, "randomCategoryShortName", x);
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
